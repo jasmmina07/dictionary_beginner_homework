eac8989fd137fc2d1bfb7e155880622eb045bbbf
Jasmina <jasminatarmasheva007@gmail.com>
2023-07-14 18:12:29 +0500

    Finished

--- find_max_key.py
+++ find_max_key.py
@@ -1,3 +1,8 @@
+data = {
+    1: 'a', 
+    2: 'b', 
+    3: 'c'
+  }
 def find_max_key(data: dict):
     """
     Return the maximum int or float key in a dictionary.
@@ -6,4 +11,5 @@ def find_max_key(data: dict):
     Returns:
         int: The maximum key in the dictionary.
     """
-    return 
+    return max(data.keys())
+print(find_max_key(data))
