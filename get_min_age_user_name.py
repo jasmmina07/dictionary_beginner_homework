c9cc055d1e6afbd67fc0117a25966583da2e59ca
Jasmina <jasminatarmasheva007@gmail.com>
2023-07-14 18:21:46 +0500

    Finished

--- get_min_age_user_name.py
+++ get_min_age_user_name.py
@@ -1,3 +1,13 @@
+data = [
+  {
+    'name': 'John', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+]
 def get_min_age_user_name(data:list) -> str:
     """
     Return the name of the user with the minimum age in a dictionary.
@@ -7,4 +17,11 @@ def get_min_age_user_name(data:list) -> str:
     Returns:
         str: The name of the user with the minimum age in the dictionary
     """
-    return 
+    age=data[0]['age']
+    name=''
+    for i in data:
+        if i['age']<=age:
+            age=i['age']
+            name=i['name']
+    return name
+print(get_min_age_user_name(data))
