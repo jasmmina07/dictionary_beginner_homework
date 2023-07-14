b8c7c77caa057e27148992bec899462ff88ee503
Jasmina <jasminatarmasheva007@gmail.com>
2023-07-14 22:49:00 +0500

    Finished

--- get_user_names.py
+++ get_user_names.py
@@ -1,3 +1,14 @@
+data = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  }
+]
+country = "USA"
 def get_user_names(data:list, country:str) -> list:
     """
     Return a list of users with a given country
@@ -8,4 +19,9 @@ def get_user_names(data:list, country:str) -> list:
     Returns:
         list: A list of users with the given country
     """
-    return 
+    ans=[]
+    for i in data:
+        if i['country']==country:
+            ans.append(i['name'])
+    return ans
+print(get_user_names(data, country))
