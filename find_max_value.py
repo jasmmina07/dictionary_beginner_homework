951585bd716af57fe1a58550da1a4f0d859654ee
Jasmina <jasminatarmasheva007@gmail.com>
2023-07-14 18:14:58 +0500

    Finished

--- find_max_value.py
+++ find_max_value.py
@@ -1,3 +1,8 @@
+data = {
+    'a' : 1, 
+    'b' : 2, 
+    'c' : 3
+}
 def find_max_value(data: dict):
     """
     Return the maximum int or float value in a dictionary.
@@ -6,4 +11,9 @@ def find_max_value(data: dict):
     Returns:
         int: The maximum value in the dictionary.
     """
-    return 
+    ans=0
+    for i in data:
+        if data[i]>ans:
+            ans=data[i]
+    return ans
+print(find_max_value(data))
